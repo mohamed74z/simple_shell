44cce100f451b02ebb535c76f6c9e5b083d4d04b
mo7amed74 <mo7amed74@gmail.com>
2023-05-24 17:04:29 -0700

    74

--- calloc.c
+++ calloc.c
@@ -1,29 +1,71 @@
-#include <stdlib.h>
 #include "shell.h"
-/**
- * _calloc -  takes two arguments, the number of elements to allocate memory
- * @nmemb: number of elements
- * @size: the size of each element in bytes
- * Return: nothing or zero
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	void *p = NULL;
-	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-	{
-		return (NULL);
-	}
-	p = malloc(nmemb * size);
-	if (p == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < (nmemb * size); i++)
-	{
-		*((char *)(p) + i) = 0;
-	}
-	return (p);
+/**
+ * inter_active - returns true if shell is interactive mode
+ * @info: struct address
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+int inter_active(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readpd <= 2);
 }
 
+/**
+ * _delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int _delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ * _alpha -  checks for alphabetic checker
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+int _alpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - converts a string to an interger
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ */
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result  = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
